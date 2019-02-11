ac25a9583256b887d1e048dfefac9fdb49ea7f95
Mika Westphal <westphal.mika@freenet.de>
2019-02-11 14:21:08 +0100

    Mehr unterprogramme

--- guess-number.py
+++ guess-number.py
@@ -2,53 +2,51 @@ import random, os, sys
 
 linesep = os.linesep
 
-def startgame():
+def numberinput(usermessage: str, errormessage: str, whileone: bool):
+    exitWhile = False
+    while not exitWhile:        
+        userinput = str(input(usermessage))
+        isinputdecimal = bool(userinput.isdecimal())
+        if isinputdecimal:
+            return int(userinput)
+        else:
+            print(errormessage)
+            if whileone:
+                exitWhile = True
+
+def getrange(numberone: int, numbertwo: int):
+    if numberone > numbertwo:
+        returnlist = [numbertwo, numberone]
+        return returnlist
+    elif numberone < numbertwo:
+        returnlist = [numberone, numbertwo]
+        return returnlist 
+
+def game():
     print("Willkommen zum Spiel rate die Zahl!{}".format(linesep))
     print("Bitte gebe den Zahlen bereich ein in welchem du Spielen möchtest!{}".format(linesep))
 
     # Die Zahlen Reichweite mit dem Boolean wert ob es eine Zahl ist
-    numberRange = [0, False, 0, False]
-
-    while not numberRange[1]:
-        tmpMinimalValue = input("Minimum: ")
-
-        if tmpMinimalValue.isdecimal():
-            numberRange[0] = int(tmpMinimalValue)
-            numberRange[1] = True            
-        else:
-            print("{}Bitte gebe eine Gültige Zahl ein!{}".format(linesep, linesep))
-
-    while not numberRange[3]:
-        tmpMaximalValue = input("Maximum: ")
-
-        if tmpMaximalValue.isdecimal():
-            if not int(tmpMaximalValue) < numberRange[0]:
-                numberRange[2] = int(tmpMaximalValue)
-                numberRange[3] = True
-            else:
-                print("{}Deine Maximale Zahl darf nicht kleiner sein als die Minimale Zahl!{}".format(linesep, linesep))
-        else:
-            print("{}Bitte gebe eine Gültige Zahl ein!{}".format(linesep, linesep))
+    rangeNumber1 = numberinput("Minimum: ", "{}Bitte gebe eine Gültige Zahl ein!{}".format(linesep, linesep), False)
+    rangeNumber2 = numberinput("Maximum: ", "{}Bitte gebe eine Gültige Zahl ein!{}".format(linesep, linesep), False)
+    
+    numberRange = getrange(rangeNumber1, rangeNumber2)
 
     # Hier werden die Spielinformationen gespeichert, wie z.B. die Zahl und die Versuche
     # sowie ein Boolean wert zum beenden des Spiels
-    gameValues = [random.randint(numberRange[0], numberRange[2]), 0, False]
+    gameValues = [random.randint(numberRange[0], numberRange[1]), 0, False]
 
     while not gameValues[2]:
-        userInput = input("{}Bitte gebe deine Zahl ein: ".format(linesep))
+        userInput = numberinput("{}Bitte gebe deine Zahl ein: ".format(linesep), "{}Bitte gebe eine Gültige Zahl ein!".format(linesep), False)
+        
+        gameValues[1] = gameValues[1] + 1
 
-        if userInput.isdecimal():
-            gameValues[1] = gameValues[1] + 1
-            userInputValue = int(userInput)
-
-            if userInputValue == gameValues[0]:
-                gameValues[2] = True
-            elif userInputValue < gameValues[0]:
-                print("{}Die Zahl ist größer!".format(linesep))
-            elif userInputValue > gameValues[0]:
-                print("{}Die Zahl ist kleiner!".format(linesep))
-        else:
-            print("{}Bitte gebe eine Gültige Zahl ein!".format(linesep))
+        if userInput == gameValues[0]:
+            gameValues[2] = True
+        elif userInput < gameValues[0]:
+            print("{}Die Zahl ist größer!".format(linesep))
+        elif userInput > gameValues[0]:
+            print("{}Die Zahl ist kleiner!".format(linesep))
 
     print("Herzlichen Glückwunsch, du hast die Zahl {} nach {} versuchen erraten!".format(gameValues[0], gameValues[1]))
     input()
@@ -66,10 +64,8 @@ while True:
     print("1) Spiel starten")
     print("2) Spiel beenden")
     
-    menupunkt = input()
-    if menupunkt.isdecimal():
-        menupunkt = int(menupunkt)
-        if menupunkt == 1:
-            startgame()
-        elif menupunkt == 2:
-            sys.exit()
+    menupunkt = numberinput("", "", True)
+    if menupunkt == 1:
+        game()
+    elif menupunkt == 2:
+        sys.exit()
